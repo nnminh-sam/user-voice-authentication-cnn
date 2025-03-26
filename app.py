bc152f0986112e981c8c3ebb8e73e199521b95f1
nnminh-haise <nnminh.sam.1803@gmail.com>
2025-03-26 08:31:16 +0700

    update: saved current model for analysis. model optimized for size. ready for embeded to esp32

--- app.py
+++ app.py
@@ -9,41 +9,42 @@ from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 import utils.data_processer as DataProcesser
 import utils.logger as Logger
-import model.model as Model
+import utils.model_conversion as ModelConversion
+import model.model as Models
 
 
 def read_data_and_extract_feature(dataset_path: str):
     """Read data from dataset folder and extract features from each audio data"""
     features = []
     labels = []
-    
+
     for label_folder in os.listdir(dataset_path):
         label_folder_path = os.path.join(dataset_path, label_folder)
-        
+
         if not os.path.isdir(label_folder_path):
             continue
-        
+
         for file_name in os.listdir(label_folder_path):
             if not file_name.endswith(".bin"):
                 Logger.log(f"Skipped file: {file_name}")
                 continue
-            
+
             Logger.log(f"Processing file: {file_name}")
             file_path = os.path.join(label_folder_path, file_name)
-            
+
             data, sample_rate = DataProcesser.read_pcm_binary(file_path)
             Logger.log(f"Data from {file_name} is read")
-            
+
             feature_vector = DataProcesser.extract_features(data, sample_rate)
             features.append(feature_vector)
             labels.append(label_folder)
-    
+
     unique_labels = sorted(list(set(labels)))  # Sort to maintain consistency
     label_map = {name: i for i, name in enumerate(unique_labels)}
     reverse_label_map = {i: name for name, i in label_map.items()}  # Reverse mapping
-    
+
     numeric_labels = np.array([label_map[name] for name in labels])
-    
+
     return np.array(features), numeric_labels, label_map, reverse_label_map
 
 
@@ -59,8 +60,12 @@ def split_and_save_sets(features, file_names, train_path, val_path, test_path):
     """
 
     # * 70% Train, 15% Validation, 15% Test
-    X_train, X_temp, y_train, y_temp = train_test_split(features, file_names, test_size=0.3, random_state=42)
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        features, file_names, test_size=0.3, random_state=42
+    )
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp, y_temp, test_size=0.5, random_state=42
+    )
 
     os.makedirs(train_path, exist_ok=True)
     os.makedirs(val_path, exist_ok=True)
@@ -75,7 +80,9 @@ def split_and_save_sets(features, file_names, train_path, val_path, test_path):
     np.save(os.path.join(val_path, "y_val.npy"), y_val)
     np.save(os.path.join(test_path, "y_test.npy"), y_test)
 
-    Logger.log(f"Dataset split: Train={len(X_train)}, Val={len(X_val)}, Test={len(X_test)}")
+    Logger.log(
+        f"Dataset split: Train={len(X_train)}, Val={len(X_val)}, Test={len(X_test)}"
+    )
 
     return (
         (X_train, y_train),
@@ -96,24 +103,26 @@ def authenticate_user(voice_path, cnn_model, label_map_path, threshold=0.9):
     Returns:
         _type_: _description_
     """
-    
+
     Logger.log(f"Identifing {voice_path}")
     with open(label_map_path, "r") as f:
         reverse_label_map = json.load(f)
 
     data, sample_rate = DataProcesser.read_pcm_binary(voice_path)
     Logger.log(f"Data from {voice_path} is read")
-    
+
     feature_vector = DataProcesser.extract_features(data, sample_rate)
     feature_vector = feature_vector.reshape(1, 16, 1)
-    
+
     # Predict using trained CNN model
     predictions = cnn_model.predict(feature_vector)
     predicted_index = np.argmax(predictions)
     confidence = np.max(predictions)
-    
-    predicted_label = reverse_label_map[str(predicted_index)]  # Convert key to string since JSON keys are strings
-    
+
+    predicted_label = reverse_label_map[
+        str(predicted_index)
+    ]  # Convert key to string since JSON keys are strings
+
     if confidence >= threshold:
         print(f"Authenticated as {predicted_label} (Confidence: {confidence:.2f})")
         return predicted_label
@@ -129,9 +138,13 @@ def main():
     TRAIN_SET_PATH: str = "features/train"
     VALIDATION_SET_PATH: str = "features/validation"
     TEST_SET_PATH: str = "features/test"
-    USAGE_TEST_PATH: str = "dataset/anh_ban_than/vai_dieu_can_thiet_truoc_khi_ket_hon_00.bin"
-    
-    features, labels, label_map, reverse_label_map = read_data_and_extract_feature(DATASET_PATH)
+    USAGE_TEST_PATH: str = "dataset/nnminh_test/audio_1741766932066.bin"
+    C_MODEL_NAME: str = "voice_model"
+    TFLITE_MODEL_NAME: str = "voice_model"
+
+    features, labels, label_map, reverse_label_map = read_data_and_extract_feature(
+        DATASET_PATH
+    )
 
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = split_and_save_sets(
         features, labels, TRAIN_SET_PATH, VALIDATION_SET_PATH, TEST_SET_PATH
@@ -142,53 +155,65 @@ def main():
     os.makedirs("output", exist_ok=True)
     with open(LABEL_MAP_PATH, "w") as f:
         json.dump(reverse_label_map, f)
-    
+
     num_classes = len(set(labels))
     y_train = to_categorical(y_train, num_classes)
     y_val = to_categorical(y_val, num_classes)
     y_test = to_categorical(y_test, num_classes)
-    
+
     X_train = X_train.reshape(X_train.shape[0], 16, 1)
     X_val = X_val.reshape(X_val.shape[0], 16, 1)
     X_test = X_test.reshape(X_test.shape[0], 16, 1)
 
-    
     print("X_train shape:", X_train.shape)
     print("X_val shape:", X_val.shape)
     print("X_test shape:", X_test.shape)
 
     callbacks = [
-        EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True),
-        ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6)
+        EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True),
+        ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=5, min_lr=1e-6),
     ]
 
-    model = Model.build_voice_auth_cnn(num_classes)
+    # model = Model.build_voice_auth_cnn(num_classes)
+    model = Models.CNNModelV2.build(num_classes)
     model.summary()
     model.compile(
-        loss='categorical_crossentropy',
+        loss="categorical_crossentropy",
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-        metrics=['accuracy']
+        metrics=["accuracy"],
     )
-    
-    # history = model.fit(
-    #     X_train, y_train,
-    #     validation_data=(X_val, y_val),
-    #     epochs=100,
-    #     batch_size=32
-    # )
-    history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32, callbacks=callbacks)
-    
+
+    history = model.fit(
+        X_train,
+        y_train,
+        validation_data=(X_val, y_val),
+        epochs=100,
+        batch_size=32,
+        callbacks=callbacks,
+    )
+
     test_loss, test_acc = model.evaluate(X_test, y_test)
     Logger.log(f"Test Accuracy: {test_acc:.2f}")
-    
-    plt.plot(history.history['accuracy'], label='Train Accuracy')
-    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+
+    plt.plot(history.history["accuracy"], label="Train Accuracy")
+    plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
     plt.xlabel("Epochs")
     plt.ylabel("Accuracy")
     plt.legend()
     plt.savefig("output/model.png")
 
     authenticate_user(USAGE_TEST_PATH, model, LABEL_MAP_PATH)
+
+    # Convert Keras model to a tflite model
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+    tflite_model = converter.convert()
+
+    open(TFLITE_MODEL_NAME + ".tflite", "wb").write(tflite_model)
+
+    # Write TFLite model to a C source (or header) file
+    with open(C_MODEL_NAME + ".h", "w") as file:
+        file.write(ModelConversion.hex_to_c_array(tflite_model, C_MODEL_NAME))
 
 
 main()
--- model/model.py
+++ model/model.py
@@ -4,75 +4,60 @@ from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, LeakyReLU, Dropou
 from tensorflow.keras.regularizers import l2
 
 
-# * Version 1
-# def build_voice_auth_cnn(num_classes):
-#     model = Sequential()
+class CNNModel:
+    def build(num_classes) -> Sequential:
+        model = Sequential([
+            Conv1D(32, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001), input_shape=(16, 1)),
+            LeakyReLU(alpha=0.1),
+            MaxPooling1D(pool_size=2),
 
-#     # Input shape: (time_steps, features) -> (None, 16, 1)
-#     input_shape = (16, 1)  # 16 extracted features, 1D signal
+            Conv1D(64, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001)),
+            LeakyReLU(alpha=0.1),
+            MaxPooling1D(pool_size=2),
 
-#     # First Conv Layer
-#     model.add(Conv1D(filters=32, kernel_size=3, padding='same', input_shape=input_shape))
-#     model.add(LeakyReLU(alpha=0.3))
-#     model.add(MaxPooling1D(pool_size=2))
+            Conv1D(128, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001)),
+            LeakyReLU(alpha=0.1),
+            MaxPooling1D(pool_size=2),
 
-#     # Second Conv Layer
-#     model.add(Conv1D(filters=64, kernel_size=3, padding='same'))
-#     model.add(LeakyReLU(alpha=0.3))
-#     model.add(MaxPooling1D(pool_size=2))
+            Flatten(),
+            Dense(64, kernel_regularizer=l2(0.001)), 
+            LeakyReLU(alpha=0.1),
+            Dropout(0.3),
 
-#     # Third Conv Layer
-#     model.add(Conv1D(filters=128, kernel_size=3, padding='same'))
-#     model.add(LeakyReLU(alpha=0.3))
-#     model.add(MaxPooling1D(pool_size=2))
+            Dense(num_classes, activation='softmax')
+        ])
 
-#     # Flatten Layer
-#     model.add(Flatten())
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-#     # Fully Connected Dense Layer
-#     model.add(Dense(128))
-#     model.add(LeakyReLU(alpha=0.3))
-#     model.add(Dropout(0.3))  # Prevent overfitting
+        return model
 
-#     # Output Layer with Softmax
-#     model.add(Dense(num_classes, activation='softmax'))
-
-#     # Compile the model
-#     model.compile(
-#         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-#         loss='categorical_crossentropy',
-#         metrics=['accuracy']
-#     )
-
-#     return model
 
 
 # ----
 
-# * Version 2
-def build_voice_auth_cnn(num_classes) -> Sequential:
-    model = Sequential([
-        Conv1D(32, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001), input_shape=(16, 1)),
-        LeakyReLU(alpha=0.1),
-        MaxPooling1D(pool_size=2),
+class CNNModelV2:
+    def build(num_classes) -> Sequential:
+        model = Sequential([
+            Conv1D(16, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001), input_shape=(16, 1)),
+            LeakyReLU(alpha=0.1),
+            MaxPooling1D(pool_size=2),
 
-        Conv1D(64, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001)),
-        LeakyReLU(alpha=0.1),
-        MaxPooling1D(pool_size=2),
+            Conv1D(32, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001)),
+            LeakyReLU(alpha=0.1),
+            MaxPooling1D(pool_size=2),
 
-        Conv1D(128, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001)),
-        LeakyReLU(alpha=0.1),
-        MaxPooling1D(pool_size=2),
+            Conv1D(64, kernel_size=3, activation=None, padding='same', kernel_regularizer=l2(0.001)),
+            LeakyReLU(alpha=0.1),
+            MaxPooling1D(pool_size=2),
 
-        Flatten(),
-        Dense(64, kernel_regularizer=l2(0.001)), 
-        LeakyReLU(alpha=0.1),
-        Dropout(0.3),
+            Flatten(),
+            Dense(64, kernel_regularizer=l2(0.001)), 
+            LeakyReLU(alpha=0.1),
+            Dropout(0.3),
 
-        Dense(num_classes, activation='softmax')
-    ])
+            Dense(num_classes, activation='softmax')
+        ])
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    return model
-
+        return model
